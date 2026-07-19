7edbc2024e20e24f3890e939b17b91aedbe400a0
MrSampaio <jusampa2@gmail.com>
2026-07-19 20:12:57 -0300

    feat: implements navigation views and complete tabview component

---
+++ Clink/View/AllListsView.swift
@@ -0,0 +1,15 @@
+//
+//  AllListsView.swift
+//  Clink
+//
+//  Created by Julio Sampaio on 19/07/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct AllListsView: View {
+    var body: some View {
+        Text("All Lists")
+    }
+}
---
+++ Clink/View/ListView.swift
@@ -0,0 +1,15 @@
+//
+//  ListView.swift
+//  Clink
+//
+//  Created by Julio Sampaio on 19/07/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("ListView")
+    }
+}
