ffed639ec31f1c75ec62b84124602c3cd10c6330
vitorss <226780885+vitorssza@users.noreply.github.com>
2026-07-24 02:20:55 -0300

    feat: create manageview fix: icon toolbar-widget

--- Clink/View/Components/ToolBar.swift
+++ Clink/View/Components/ToolBar.swift
@@ -165,7 +165,7 @@ struct ManageToolBar: ToolbarContent {
         
         ToolbarItem(placement: .navigationBarTrailing) {
             Button(action: {
-                print ("Menu Clicada") }) { Image(systemName: "elliipsis")}
+                print ("Menu Clicada") }) { Image(systemName: "ellipsis")}
         }
     }
 }
--- Clink/View/ManageView.swift
+++ Clink/View/ManageView.swift
@@ -9,7 +9,56 @@ import Foundation
 import SwiftUI
 
 struct ManageView: View {
-    var body: some View {
-        Text("Manage")
+    @State private var selectedTab = 0
+    
+    var currentCount: String {
+        switch selectedTab {
+        case 0: return "15"
+        case 1: return "2"
+        case 2: return "4"
+        default: return "0"
+        }
     }
+    
+    var currentDescription: String {
+        switch selectedTab {
+        case 0: return "Lembretes concluídos"
+        case 1: return "Lembretes apagados"
+        case 2: return "Lembretes trancados"
+        default: return ""
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                
+                Title(title: "Gerenciar", subtitle: "")
+                    .padding(16)
+                VStack {
+                    Picker("dada", selection: $selectedTab) {
+                        Text("Concluidos").tag(0)
+                        Text("Apagados").tag(1)
+                        Text("Trancados").tag(2)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding(16)
+                }
+                VStack (spacing: 12) {
+                    Text(currentCount)
+                        .font(.system(size: 41, weight: .bold))
+                    Text(currentDescription)
+                }
+                .padding(38)
+            }
+            .toolbar {
+                ManageToolBar()
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ManageView()
 }
