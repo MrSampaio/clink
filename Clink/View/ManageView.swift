389e1c5daf067a3e0190dd0c0cd89f25d656b843
MrSampaio <jusampa2@gmail.com>
2026-07-27 17:10:05 -0300

    feat: implements search system into manage view

--- Clink/View/ManageView.swift
+++ Clink/View/ManageView.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Julio Sampaio on 19/07/26.
 //
+//
+//  ManageView.swift
+//  Clink
+//
+//  Created by Julio Sampaio on 19/07/26.
+//
 
 import SwiftUI
 import LocalAuthentication
@@ -13,6 +19,8 @@ struct ManageView: View {
     @EnvironmentObject var viewModel: ReminderViewModel
     @StateObject private var securityVM = SecurityViewModel()
     @State private var selectedPicker = 0
+    
+    @State private var searchText = ""
     
     var currentCount: String {
         switch selectedPicker {
@@ -32,6 +40,29 @@ struct ManageView: View {
         }
     }
     
+    var filteredConcludedIndices: [Int] {
+        viewModel.concludedRemindersIndices.filter { index in
+            let r = viewModel.reminders[index]
+            return r.title.localizedCaseInsensitiveContains(searchText) ||
+                   (r.description?.localizedCaseInsensitiveContains(searchText) ?? false)
+        }
+    }
+    
+    var filteredDeleted: [Reminder] {
+        viewModel.deletedReminders.filter { r in
+            return r.title.localizedCaseInsensitiveContains(searchText) ||
+                   (r.description?.localizedCaseInsensitiveContains(searchText) ?? false)
+        }
+    }
+    
+    var filteredLockedIndices: [Int] {
+        viewModel.lockedRemindersIndices.filter { index in
+            let r = viewModel.reminders[index]
+            return r.title.localizedCaseInsensitiveContains(searchText) ||
+                   (r.description?.localizedCaseInsensitiveContains(searchText) ?? false)
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             Group {
@@ -46,8 +77,8 @@ struct ManageView: View {
                     ManageToolBar()
                 }
             }
+            //.searchable(text: $searchText, prompt: "Buscar lembretes...")
         }
-        
         .onAppear {
             if !securityVM.isAuthenticated {
                 securityVM.authenticate()
@@ -58,51 +89,120 @@ struct ManageView: View {
         }
     }
     
-    // MARK: - Tela Principal (Conteúdo Liberado)
     @ViewBuilder
     var mainContent: some View {
         ScrollView {
-            Title(title: "Gerenciar", subtitle: "Visualize seus lembretes concluídos, apagados ou trancados")
-                .padding(16)
             
-            VStack {
-                Picker("FilterManage", selection: $selectedPicker) {
-                    Text("Concluídos").tag(0)
-                    Text("Apagados").tag(1)
-                    Text("Trancados").tag(2)
-                }
-               
-                .pickerStyle(.segmented)
-                .padding(16)
-            }
-            
-            VStack(spacing: 12) {
-                Text(currentCount)
-                    .font(.system(size: 41, weight: .bold))
-                Text(currentDescription)
+            if searchText.isEmpty {
                 
-                if selectedPicker == 0 {
-                    ForEach(viewModel.concludedRemindersIndices, id: \.self) { index in
-                        ReminderCard(reminder: $viewModel.reminders[index], enableEdit: false)
-                            .padding(.top, 15)
+                Title(title: "Gerenciar", subtitle: "Visualize seus lembretes concluídos, apagados ou trancados")
+                    .padding(16)
+                
+                VStack {
+                    Picker("FilterManage", selection: $selectedPicker) {
+                        Text("Concluídos").tag(0)
+                        Text("Apagados").tag(1)
+                        Text("Trancados").tag(2)
                     }
-                } else if selectedPicker == 1 {
-                    ForEach($viewModel.deletedReminders) { $deletedReminder in
-                        ReminderCard(reminder: $deletedReminder, enableEdit: false)
-                            .padding(.top, 15)
-                    }
-                } else if selectedPicker == 2 {
-                    ForEach(viewModel.lockedRemindersIndices, id: \.self) { index in
-                        ReminderCard(reminder: $viewModel.reminders[index], forceUnlock: true)
-                            .padding(.top, 15)
+                    .pickerStyle(.segmented)
+                    .padding(16)
+                }
+                
+                VStack(spacing: 12) {
+                    Text(currentCount)
+                        .font(.system(size: 41, weight: .bold))
+                    Text(currentDescription)
+                    
+                    if selectedPicker == 0 {
+                        if viewModel.concludedRemindersIndices.isEmpty {
+                           Text("Nenhum lembrete concluído foi encontrado.")
+                                .padding(.top, 40)
+                        } else{
+                            ForEach(viewModel.concludedRemindersIndices, id: \.self) { index in
+                                ReminderCard(reminder: $viewModel.reminders[index], enableEdit: false)
+                                    .padding(.top, 15)
+                            }
+                        }
+                        
+                    } else if selectedPicker == 1 {
+                        if viewModel.deletedReminders.isEmpty {
+                            Text("Nenhum lembrete apagado foi encontrado.")
+                                 .padding(.top, 40)
+                        } else{
+                            ForEach($viewModel.deletedReminders) { $deletedReminder in
+                                ReminderCard(reminder: $deletedReminder, enableEdit: false)
+                                    .padding(.top, 15)
+                            }
+                        }
+                        
+                    } else if selectedPicker == 2 {
+                        if viewModel.lockedRemindersIndices.isEmpty {
+                            Text("Nenhum lembrete trancado foi encontrado.")
+                                 .padding(.top, 40)
+                        } else{
+                            ForEach(viewModel.lockedRemindersIndices, id: \.self) { index in
+                                ReminderCard(reminder: $viewModel.reminders[index], forceUnlock: true)
+                                    .padding(.top, 15)
+                            }
+                        }
+                        
                     }
                 }
+                .padding(.horizontal, 5)
+                
+            } else {
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    if filteredConcludedIndices.isEmpty && filteredDeleted.isEmpty && filteredLockedIndices.isEmpty {
+                        Text("Nenhum lembrete encontrado.")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.top, 40)
+                    } else {
+                        
+                        if !filteredConcludedIndices.isEmpty {
+                            Text("Encontrados em Concluídos")
+                                .font(.title3.bold())
+                                .padding(.horizontal, 10)
+                            
+                            ForEach(filteredConcludedIndices, id: \.self) { index in
+                                ReminderCard(reminder: $viewModel.reminders[index], enableEdit: false)
+                            }
+                        }
+                        
+                        if !filteredDeleted.isEmpty {
+                            Text("Encontrados em Apagados")
+                                .font(.title3.bold())
+                                .padding(.horizontal, 10)
+                                .padding(.top, 10)
+                            
+                            ForEach(filteredDeleted, id: \.id) { deletedReminder in
+                                if let index = viewModel.deletedReminders.firstIndex(where: { $0.id == deletedReminder.id }) {
+                                    ReminderCard(reminder: $viewModel.deletedReminders[index], enableEdit: false)
+                                }
+                            }
+                        }
+                        
+                        if !filteredLockedIndices.isEmpty {
+                            Text("Encontrados em Trancados")
+                                .font(.title3.bold())
+                                .padding(.horizontal, 10)
+                                .padding(.top, 10)
+                            
+                            ForEach(filteredLockedIndices, id: \.self) { index in
+                                ReminderCard(reminder: $viewModel.reminders[index], forceUnlock: true)
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal, 5)
             }
-            .padding(.horizontal, 5)
-        } .background(Color(.background))
+        }
+        .background(Color(.background))
+        .searchable(text: $searchText, prompt: "Buscar lembretes...")
     }
     
-    // lógica da tela de bloqueio
     @ViewBuilder
     var lockedContent: some View {
         VStack(spacing: 20) {
@@ -134,9 +234,8 @@ struct ManageView: View {
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.background))
-    }    
+    }
 }
-
 #Preview {
     ManageView()
 }
