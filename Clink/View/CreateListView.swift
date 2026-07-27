922294b144e4fa0d3097cf8f79bfbacf60c2cf9f
vitorss <226780885+vitorssza@users.noreply.github.com>
2026-07-27 17:20:20 -0300

    feat: add CreateListView

--- Clink/View/CreateListView.swift
+++ Clink/View/CreateListView.swift
@@ -9,7 +9,36 @@ import Foundation
 import SwiftUI
 
 struct CreateListView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var listName: String = ""
+    @State private var selectedIcon: String = "list.bullet"
+    @State private var selectedColor: Int = 0
+    
+    let palette: [LinearGradient] = [
+        .grayGradient, .redGradient, .orangeGradient, .yellowGradient, .greenGradient, .blueGradient, .indigoGradient, .purpleGradient, .pinkGradient, .brownGradient]
+    
     var body: some View {
-        Text("Create List View")
+        
+        NavigationStack {
+            Form {
+                IconAndTitleList(
+                    listName: $listName,
+                    selectedIcon: selectedIcon,
+                    selectedGradient: palette[selectedColor % palette.count]
+                )
+                
+                PresetsList()
+                
+                ColorsList(selectedColor: $selectedColor, palette: palette)
+                
+                IconList(selectedIcon: $selectedIcon, selectedGradient: palette[selectedColor])
+            }
+        }
     }
+}
+
+#Preview {
+    CreateListView()
 }
